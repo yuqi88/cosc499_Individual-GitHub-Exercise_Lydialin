264650777722c5c468618e02a02e3f9ee36dfbc0
yuqi88 <snow34545@gmail.com>
2022-09-15 11:08:02 -0700

    sort array

--- individual_github_exercise.java
+++ individual_github_exercise.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
+
 public class individual_github_exercise {
     public static void main(String args[])
     {
 
+    }
+    public static void sortArray(int[] arr){
+        Arrays.sort(arr);
     }
 }
