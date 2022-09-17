36edd60b8c70a21a13e20ac328f04d863b1af656
yuqi88 <snow34545@gmail.com>
2022-09-16 20:28:05 -0700

    add test

--- individual_github_exercise.java
+++ individual_github_exercise.java
@@ -3,7 +3,10 @@ import java.util.Arrays;
 public class individual_github_exercise {
     public static void main(String args[])
     {
-
+        int[] testArray = {7,3,4,5,2,1};
+        sortArray(testArray);
+        for(int i=0; i<testArray.length; i++)
+            System.out.print(testArray[i] + " ");
     }
     public static void sortArray(int[] arr){
         Arrays.sort(arr);
