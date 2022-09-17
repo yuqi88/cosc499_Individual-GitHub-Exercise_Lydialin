9a9e91715397e9fcbdd66def5b70727d4aec05aa
yuqi88 <snow34545@gmail.com>
2022-09-16 20:54:45 -0700

    for recording

--- individual_github_exercise.java
+++ individual_github_exercise.java
@@ -7,6 +7,7 @@ public class individual_github_exercise {
         sortArray(testArray);
         for(int i=0; i<testArray.length; i++)
             System.out.print(testArray[i] + " ");
+        System.out.println("for recording yay");
     }
     public static void sortArray(int[] arr){
         Arrays.sort(arr);
